44e501f4710a052c524560a33148554c5fd7e0d1
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-09-26 20:16:59 -1000

    Remove unused pseudocode from js file

--- script.js
+++ script.js
@@ -16,23 +16,3 @@ function checkPasswordsMatch () {
         return true;
     }
 }
-
-
-/* 
--add event listener to submit button
--if the password and confirm-password field, OR one or the other, is empty, return
--if the password and confirm-password field is NOT EMPTY, run the checkPassWordMatch function
-*/
-
-
-// const password = document.getElementById("password").value;
-// const confirmPassword = document.getElementById("confirm-password").value;
-
-// if (password != "" & confirmPassword != "") {
-//     if (password === confirmPassword) {
-//         console.log("Passwords match")
-//     }
-//     if (password !== confirmPassword) {
-//         console.log("Passwords don't match")
-//     }
-// }
